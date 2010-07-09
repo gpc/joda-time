f03fcc34f6f03b502865b6dc0c963234a6a38196
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-07-09 16:59:30 +0100

    push version number

--- JodaTimeGrailsPlugin.groovy
+++ JodaTimeGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 import com.energizedwork.grails.plugins.jodatime.*
 
 class JodaTimeGrailsPlugin {
-    def version = "1.0"
+    def version = "1.1"
 	def grailsVersion = "1.1 > *"
     def dependsOn = [core: "1.1 > *", converters: "1.1 > *"]
 
