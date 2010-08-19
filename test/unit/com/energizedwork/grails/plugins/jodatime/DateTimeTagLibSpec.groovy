7fd00b79aff5fe2a9ce9f8fc7e4949a2d0011ce6
Rob Fletcher <robert.w.fletcher@gmail.com>
2010-08-19 06:47:09 +0100

    changed taglib test to spec

--- test/unit/com/energizedwork/grails/plugins/jodatime/DateTimeTagLibSpec.groovy
+++ test/unit/com/energizedwork/grails/plugins/jodatime/DateTimeTagLibSpec.groovy
@@ -15,230 +15,188 @@
  */
 package com.energizedwork.grails.plugins.jodatime
 
-import grails.test.TagLibUnitTestCase
+import spock.lang.*
+import grails.plugin.spock.*
 import org.codehaus.groovy.grails.plugins.codecs.HTMLCodec
-import org.joda.time.DateTime
-import org.joda.time.DateTimeUtils
-import org.joda.time.LocalDateTime
-import static com.energizedwork.grails.commons.test.RegexMatcher.isMatch
-import static org.hamcrest.Matchers.not
-import static org.junit.Assert.assertThat
+import org.joda.time.*
 
-class DateTimeTagLibTests extends TagLibUnitTestCase {
+class DateTimeTagLibSpec extends TagLibSpec {
 
-	void setUp() {
-		super.setUp()
-
-		String.metaClass.encodeAsHTML = {-> HTMLCodec.encode(delegate) }
-
-		def mockGrailsApplication = [config: new ConfigObject()]
-		tagLib.metaClass.getGrailsApplication = {-> mockGrailsApplication }
-		tagLib.metaClass.getOutput = {-> delegate.out.toString() }
-
-		mockRequest.addPreferredLocale Locale.UK
-
+	def setupSpec() {
 		def fixedDateTime = new DateTime(2008, 10, 2, 2, 50, 33, 0)
-		DateTimeUtils.setCurrentMillisFixed fixedDateTime.getMillis()
+		DateTimeUtils.currentMillisFixed = fixedDateTime.getMillis()
 	}
 
-	protected void tearDown() {
-		super.tearDown()
-
+	def cleanupSpec() {
 		DateTimeUtils.setCurrentMillisSystem()
 	}
 
-	void testDatePickerOutputsOnlyDateFields() {
-		tagLib.datePicker(name: 'foo')
+	def setup() {
+		loadCodec HTMLCodec
 
-		assertThat tagLib.output, isMatch(/<select name="foo_day"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_hour"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_minute"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
+		def mockGrailsApplication = [config: new ConfigObject()]
+		tagLib.metaClass.getGrailsApplication = {-> mockGrailsApplication }
+
+		mockRequest.addPreferredLocale Locale.UK
+	}
+	
+	def "date picker only outputs date fields"() {
+		when: def markup = tagLib.datePicker(name: 'foo')
+
+		then:
+		println "markup='$markup' which is a '${markup?.getClass()}'"
+		markup.contains(/<select name="foo_day"/)
+		markup.contains(/<select name="foo_month"/)
+		markup.contains(/<select name="foo_year"/)
+		!markup.contains(/<select name="foo_hour"/)
+		!markup.contains(/<select name="foo_minute"/)
+		!markup.contains(/<select name="foo_second"/)
 	}
 
-	void testTimePickerOutputsOnlyTimeFields() {
-		tagLib.timePicker(name: 'foo', precision: 'second')
+	def "time picker only outputs time fields"() {
+		when: def markup = timePicker(name: 'foo', precision: 'second')
 
-		assertThat tagLib.output, not(isMatch(/<select name="foo_day"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_month"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_year"/))
-		assertThat tagLib.output, isMatch(/<select name="foo_hour"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_minute"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_second"/)
+		then:
+		println "markup='$markup' which is a '${markup?.getClass()}'"
+		!markup.contains(/<select name="foo_day"/)
+		!markup.contains(/<select name="foo_month"/)
+		!markup.contains(/<select name="foo_year"/)
+		markup.contains(/<select name="foo_hour"/)
+		markup.contains(/<select name="foo_minute"/)
+		markup.contains(/<select name="foo_second"/)
 	}
 
-	void testPickerTagsUseCurrentDateTimeAsDefault() {
-		tagLib.dateTimePicker(name: 'foo', precision: 'second')
+	def "picker tags use current date-time as default value"() {
+		when: def markup = dateTimePicker(name: 'foo', precision: 'second')
 
-		assertThat tagLib.output, isMatch(/<option value="2" selected="selected">2<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="10" selected="selected">October<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="2008" selected="selected">2008<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="2" selected="selected">2<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="50" selected="selected">50<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="33" selected="selected">33<\/option>/)
+		then:
+		markup.contains(/<option value="2" selected="selected">2<\/option>/)
+		markup.contains(/<option value="10" selected="selected">October<\/option>/)
+		markup.contains(/<option value="2008" selected="selected">2008<\/option>/)
+		markup.contains(/<option value="2" selected="selected">2<\/option>/)
+		markup.contains(/<option value="50" selected="selected">50<\/option>/)
+		markup.contains(/<option value="33" selected="selected">33<\/option>/)
 	}
 
-	void testPickerTagsAcceptStringDefault() {
-		tagLib.dateTimePicker(name: 'foo', 'default': '1971-11-29T16:22')
+	@Unroll("dateTimePicker accepts a #type default")
+	def "dateTimePicker accepts a default"() {
+		when: def markup = dateTimePicker(name: 'foo', 'default': defaultValue)
 
-		assertThat tagLib.output, isMatch(/<option value="29" selected="selected">29<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="11" selected="selected">November<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1971" selected="selected">1971<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="16" selected="selected">16<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="22" selected="selected">22<\/option>/)
+		then:
+		markup.contains(/<option value="29" selected="selected">29<\/option>/)
+		markup.contains(/<option value="11" selected="selected">November<\/option>/)
+		markup.contains(/<option value="1971" selected="selected">1971<\/option>/)
+		markup.contains(/<option value="16" selected="selected">16<\/option>/)
+		markup.contains(/<option value="22" selected="selected">22<\/option>/)
+		
+		where:
+		defaultValue << ["1971-11-29T16:22", new DateTime(1971, 11, 29, 16, 22, 0, 0), new LocalDateTime(1971, 11, 29, 16, 22, 0, 0)]
+		type = defaultValue.getClass().simpleName
 	}
 
-	void testStringDefaultFormatIsAppropriateForPrecision() {
-		tagLib.datePicker(name: 'foo', 'default': '1971-11-29')
+	def "default value format is appropriate to precision"() {
+		when: def markup = datePicker(name: 'foo', 'default': '1971-11-29')
 
-		assertThat tagLib.output, isMatch(/<option value="29" selected="selected">29<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="11" selected="selected">November<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1971" selected="selected">1971<\/option>/)
+		then:
+		markup.contains(/<option value="29" selected="selected">29<\/option>/)
+		markup.contains(/<option value="11" selected="selected">November<\/option>/)
+		markup.contains(/<option value="1971" selected="selected">1971<\/option>/)
 	}
 
-	void testTimePickerAcceptsStringDefault() {
-		tagLib.timePicker(name: 'foo', precision: 'second', 'default': '23:59:59')
+	def "timePicker accepts a String default"() {
+		when: def markup = timePicker(name: 'foo', precision: 'second', 'default': '23:59:59')
 
-		assertThat tagLib.output, isMatch(/<option value="23" selected="selected">23<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="59" selected="selected">59<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="59" selected="selected">59<\/option>/)
+		then:
+		markup.contains(/<option value="23" selected="selected">23<\/option>/)
+		markup.contains(/<option value="59" selected="selected">59<\/option>/)
+		markup.contains(/<option value="59" selected="selected">59<\/option>/)
 	}
 
-	void testPickerTagsAcceptReadableInstantDefault() {
-		def defaultValue = new DateTime(1971, 11, 29, 16, 22, 0, 0)
-		tagLib.dateTimePicker(name: 'foo', 'default': defaultValue)
+	def "the value attribute overrides default"() {
+		when:
+		def markup = dateTimePicker(name: 'foo', 'default': defaultValue, value: value)
 
-		assertThat tagLib.output, isMatch(/<option value="29" selected="selected">29<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="11" selected="selected">November<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1971" selected="selected">1971<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="16" selected="selected">16<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="22" selected="selected">22<\/option>/)
+		then:
+		markup.contains(/<option value="8" selected="selected">8<\/option>/)
+		markup.contains(/<option value="2" selected="selected">February<\/option>/)
+		markup.contains(/<option value="1977" selected="selected">1977<\/option>/)
+		markup.contains(/<option value="09" selected="selected">09<\/option>/)
+		markup.contains(/<option value="30" selected="selected">30<\/option>/)
+		
+		where:
+		defaultValue                                  | value
+		new DateTime(1971, 11, 29, 16, 22, 0, 0)      | new DateTime(1977, 2, 8, 9, 30, 0, 0)
+		new LocalDateTime(1971, 11, 29, 16, 22, 0, 0) | new LocalDateTime(1977, 2, 8, 9, 30, 0, 0)
 	}
 
-	void testPickerTagsAcceptReadablePartialDefault() {
-		def defaultValue = new LocalDateTime(1971, 11, 29, 16, 22, 0, 0)
-		tagLib.dateTimePicker(name: 'foo', 'default': defaultValue)
+	def "picker tags accept a noSelection attribute"() {
+		when:
+		def markup = dateTimePicker(name: 'foo', noSelection: ['': 'Choose sumfink innit'])
 
-		assertThat tagLib.output, isMatch(/<option value="29" selected="selected">29<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="11" selected="selected">November<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1971" selected="selected">1971<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="16" selected="selected">16<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="22" selected="selected">22<\/option>/)
+		then:
+		markup.contains(/<select name="foo_day" id="foo_day">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1">1<\/option>/)
+		markup.contains(/<select name="foo_month" id="foo_month">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1">January<\/option>/)
+		markup.contains(/<select name="foo_year" id="foo_year">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1908">1908<\/option>/)
+		markup.contains(/<select name="foo_hour" id="foo_hour">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="00">00<\/option>/)
+		markup.contains(/<select name="foo_minute" id="foo_minute">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="00">00<\/option>/)
 	}
 
-	void testPickerTagsAcceptReadableInstantValue() {
-		def defaultValue = new DateTime(1971, 11, 29, 16, 22, 0, 0)
-		def value = new DateTime(1977, 2, 8, 9, 30, 0, 0)
-		tagLib.dateTimePicker(name: 'foo', 'default': defaultValue, value: value)
+	def "picker tags accept a years attribute"() {
+		when:
+		def markup = dateTimePicker(name: 'foo', years: 1979..1983, 'default': '1979-12-04T00:00')
 
-		assertThat tagLib.output, isMatch(/<option value="8" selected="selected">8<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="2" selected="selected">February<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1977" selected="selected">1977<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="09" selected="selected">09<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="30" selected="selected">30<\/option>/)
+		then:
+		!markup.contains(/<option value="1978">1978<\/option>/)
+		markup.contains(/<option value="1979" selected="selected">1979<\/option>/)
+		markup.contains(/<option value="1980">1980<\/option>/)
+		markup.contains(/<option value="1981">1981<\/option>/)
+		markup.contains(/<option value="1982">1982<\/option>/)
+		markup.contains(/<option value="1983">1983<\/option>/)
+		!markup.contains(/<option value="1984">1984<\/option>/)
 	}
 
-	void testPickerTagsAcceptReadablePartialValue() {
-		def defaultValue = new LocalDateTime(1971, 11, 29, 16, 22, 0, 0)
-		def value = new LocalDateTime(1942, 8, 15, 4, 16, 0, 0)
-		tagLib.dateTimePicker(name: 'foo', 'default': defaultValue, value: value)
+	def "dateTimePicker uses minute as default precision"() {
+		when:
+		def markup = dateTimePicker(name: 'foo')
 
-		assertThat tagLib.output, isMatch(/<option value="15" selected="selected">15<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="8" selected="selected">August<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1942" selected="selected">1942<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="04" selected="selected">04<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="16" selected="selected">16<\/option>/)
+		then:
+		markup.contains(/<select name="foo_minute"/)
+		!markup.contains(/<select name="foo_second"/)
 	}
 
-	void testPickerTagsAcceptNoSelectionArg() {
-		tagLib.dateTimePicker(name: 'foo', noSelection: ['': 'Choose sumfink innit'])
+	def "picker tags accept a precision attribute"() {
+		given:
+		def allFields = ["year", "month", "day", "hour", "minute", "second"]
+		
+		when:
+		def markup = dateTimePicker(name: 'foo', precision: precision)
 
-		assertThat tagLib.output, isMatch(/<select name="foo_day" id="foo_day">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1">1<\/option>/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month" id="foo_month">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1">January<\/option>/)
-		assertThat tagLib.output, isMatch(/<select name="foo_year" id="foo_year">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="1908">1908<\/option>/)
-		assertThat tagLib.output, isMatch(/<select name="foo_hour" id="foo_hour">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="00">00<\/option>/)
-		assertThat tagLib.output, isMatch(/<select name="foo_minute" id="foo_minute">\s*<option value="" selected="selected">Choose sumfink innit<\/option>\s*<option value="00">00<\/option>/)
+		then:
+		includes.every { markup.contains(/<select name="foo_$it"/) }
+		!(allFields - includes).any { markup.contains(/<select name="foo_$it"/) }
+		
+		where:
+		precision | includes
+		"year"    | ["year"]
+		"month"   | ["year", "month"]
+		"day"     | ["year", "month", "day"]
+		"hour"    | ["year", "month", "day", "hour"]
+		"minute"  | ["year", "month", "day", "hour", "minute"]
+		"second"  | ["year", "month", "day", "hour", "minute", "second"]
 	}
 
-	void testPickerTagsAcceptYearsArg() {
-		tagLib.dateTimePicker(name: 'foo', years: 1979..1983, 'default': '1979-12-04T00:00')
-
-		assertThat tagLib.output, not(isMatch(/<option value="1978">1978<\/option>/))
-		assertThat tagLib.output, isMatch(/<option value="1979" selected="selected">1979<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1980">1980<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1981">1981<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1982">1982<\/option>/)
-		assertThat tagLib.output, isMatch(/<option value="1983">1983<\/option>/)
-		assertThat tagLib.output, not(isMatch(/<option value="1984">1984<\/option>/))
-	}
-
-	void testDateTimePickerUsesMinuteAsDefaultPrecision() {
-		tagLib.dateTimePicker(name: 'foo')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_day"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_hour"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_minute"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
-	}
-
-	void testPickerTagsAcceptPrecisionArg() {
-		tagLib.dateTimePicker(name: 'foo', precision: 'year')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_month"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_day"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_hour"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_minute"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
-
-		tagLib.dateTimePicker(name: 'foo', precision: 'month')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_day"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_hour"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_minute"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
-
-		tagLib.dateTimePicker(name: 'foo', precision: 'day')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_day"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_hour"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_minute"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
-
-		tagLib.dateTimePicker(name: 'foo', precision: 'hour')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_day"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_hour"/)
-		assertThat tagLib.output, not(isMatch(/<select name="foo_minute"/))
-		assertThat tagLib.output, not(isMatch(/<select name="foo_second"/))
-
-		tagLib.dateTimePicker(name: 'foo', precision: 'second')
-
-		assertThat tagLib.output, isMatch(/<select name="foo_year"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_month"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_day"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_hour"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_minute"/)
-		assertThat tagLib.output, isMatch(/<select name="foo_second"/)
-	}
-
-	void testDateTimePickerAcceptsUseZoneArg() {
+	def "dateTimePicker accepts a zone attribute"() {
+		setup:
 		def dateTimeZoneSelectArgs
 		tagLib.metaClass.dateTimeZoneSelect = {attrs ->
 			dateTimeZoneSelectArgs = attrs
 		}
-		tagLib.dateTimePicker(name: "foo", useZone: "true")
-		assertEquals "foo_zone", dateTimeZoneSelectArgs.name
+		
+		when:
+		dateTimePicker(name: "foo", useZone: "true")
+		
+		then:
+		dateTimeZoneSelectArgs.name == "foo_zone"
 	}
 
 }
