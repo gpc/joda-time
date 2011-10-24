1d9e58d8720056e5741f995c481a72592dc538df
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-10-25 00:29:16 +0100

    fix screwy indentation

--- test/projects/joda-time-test/test/unit/jodatest/UnitTestSupportSpec.groovy
+++ test/projects/joda-time-test/test/unit/jodatest/UnitTestSupportSpec.groovy
@@ -13,24 +13,24 @@ class UnitTestSupportSpec extends Specification {
 		new Person(name: "Alex", birthday: new LocalDate(2008, 10, 2)).save(failOnError: true)
 		new Person(name: "Nicholas", birthday: new LocalDate(2010, 11, 14)).save(failOnError: true)
 	}
-	
+
 	def "sanity check"() {
 		expect:
 		Person.findByName("Alex").birthday == new LocalDate(2008, 10, 2)
 	}
-	
+
 	@Unroll
 	def "can use dynamic finders on #type properties"() {
 		expect:
 		Person.findByBirthday(new LocalDate(2008, 10, 2)).name == "Alex"
 	}
-	
+
 	def "can use criteria queries on #type properties"() {
 		when:
 		def results = Person.withCriteria {
 			gt "birthday", new LocalDate(2010, 1, 1)
 		}
-		
+
 		then:
 		results.name == ["Nicholas"]
 	}
@@ -38,33 +38,33 @@ class UnitTestSupportSpec extends Specification {
 	def "metadata for #type properties is correct"() {
 		given:
 		GrailsDomainClass dc = grailsApplication.getDomainClass(Person.name)
-        def prop = dc.getPersistentProperty("birthday")
+		def prop = dc.getPersistentProperty("birthday")
 
 		expect:
-        dc.constrainedProperties.containsKey("birthday")
+		dc.constrainedProperties.containsKey("birthday")
 		!dc.associationMap.containsKey("birthday")
 
-        and:
+		and:
 		prop != null
-        !prop.association
-        !prop.embedded
-        !prop.hasOne
-        !prop.manyToMany
-        !prop.manyToOne
-        !prop.oneToMany
-        !prop.oneToOne
-        prop.type == LocalDate
+		!prop.association
+		!prop.embedded
+		!prop.hasOne
+		!prop.manyToMany
+		!prop.manyToOne
+		!prop.oneToMany
+		!prop.oneToOne
+		prop.type == LocalDate
 	}
 
-    def "persistent entity looks right"() {
-        given:
-        println DomainClassUnitTestMixin.simpleDatastore.mappingContext.mappingSyntaxStrategy.propertyFactory
-        def entity = DomainClassUnitTestMixin.simpleDatastore.mappingContext.getPersistentEntity(Person.name)
-        def prop = entity.getPropertyByName("birthday")
+	def "persistent entity looks right"() {
+		given:
+		println DomainClassUnitTestMixin.simpleDatastore.mappingContext.mappingSyntaxStrategy.propertyFactory
+		def entity = DomainClassUnitTestMixin.simpleDatastore.mappingContext.getPersistentEntity(Person.name)
+		def prop = entity.getPropertyByName("birthday")
 
-        expect:
-        prop != null
-        prop.type == LocalDate
-    }
-	
+		expect:
+		prop != null
+		prop.type == LocalDate
+	}
+
 }
