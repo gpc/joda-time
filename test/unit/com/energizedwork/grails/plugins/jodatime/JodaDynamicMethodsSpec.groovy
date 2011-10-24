76beed04db0c9b991c0810b4b42d019900e28b1c
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-10-24 21:00:32 +0100

    restore commented out test

--- test/unit/com/energizedwork/grails/plugins/jodatime/JodaDynamicMethodsSpec.groovy
+++ test/unit/com/energizedwork/grails/plugins/jodatime/JodaDynamicMethodsSpec.groovy
@@ -15,12 +15,10 @@
  */
 package com.energizedwork.grails.plugins.jodatime
 
-import spock.lang.*
-import grails.plugin.spock.*
 import static com.energizedwork.grails.plugins.jodatime.JodaDynamicMethods.registerDynamicMethods
 import org.joda.time.*
-import static org.joda.time.DateTimeUtils.setCurrentMillisFixed
-import static org.joda.time.DateTimeUtils.setCurrentMillisSystem
+import static org.joda.time.DateTimeUtils.*
+import spock.lang.*
 
 class JodaDynamicMethodsSpec extends Specification {
 
@@ -43,7 +41,7 @@ class JodaDynamicMethodsSpec extends Specification {
 		LocalDate | "dd/MM/yyyy"          | "02/10/2008"
 		LocalTime | "HH:mm:ss"            | "02:50:00"
 	}
-	
+
 	def "multiplication operator"() {
 		expect: value * 2 == expected
 		where:
@@ -63,22 +61,25 @@ class JodaDynamicMethodsSpec extends Specification {
 		expected = type.newInstance(1)
 	}
 
-/*
-	@Test
-	void groovyOperatorsOnSingleFieldPeriods() {
-		[Days, Hours, Minutes, Months, Seconds, Weeks, Years].each { clazz ->
-			def instance = clazz.newInstance(3)
-			assertThat(-instance, equalTo(clazz.newInstance(-3)))
-			assertThat instance * 2, equalTo(clazz.newInstance(6))
-			assertThat instance / 3, equalTo(clazz.newInstance(1))
-			assertThat instance / 2, equalTo(clazz.newInstance(1)) // this is integer division
-			// the following should just work
-			assertThat instance + instance, equalTo(clazz.newInstance(6))
-			assertThat instance + 2, equalTo(clazz.newInstance(5))
-			assertThat instance - clazz.newInstance(2), equalTo(clazz.newInstance(1))
-			assertThat instance - 1, equalTo(clazz.newInstance(2))
-			assertThat instance + -instance, equalTo(clazz.newInstance(0))
-		}
+	@Unroll({"standard groovy operators work on $type.simpleName"})
+	def "groovy operators on single field periods"() {
+		given:
+		def instance = type.newInstance(3)
+
+		expect:
+		-instance == type.newInstance(-3)
+		instance * 2 == type.newInstance(6)
+		instance / 3 == type.newInstance(1)
+		instance / 2 == type.newInstance(1) // this is integer division
+		// the following should just work
+		instance + instance == type.newInstance(6)
+		instance + 2 == type.newInstance(5)
+		instance - type.newInstance(2) == type.newInstance(1)
+		instance - 1 == type.newInstance(2)
+		instance + -instance == type.newInstance(0)
+
+		where:
+		type << [Days, Hours, Minutes, Months, Seconds, Weeks, Years]
 	}
-*/
+
 }
