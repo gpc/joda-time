de39e24cc12b11a0f1dd0b98e09dbfc1628485f9
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-10-25 15:36:25 +0100

    proof of concept of unit test support

--- test/projects/joda-time-test/test/unit/jodatest/UnitTestSupportSpec.groovy
+++ test/projects/joda-time-test/test/unit/jodatest/UnitTestSupportSpec.groovy
@@ -3,13 +3,19 @@ package jodatest
 import grails.test.mixin.Mock
 import grails.test.mixin.domain.DomainClassUnitTestMixin
 import org.codehaus.groovy.grails.commons.GrailsDomainClass
+import org.grails.datastore.mapping.engine.types.AbstractMappingAwareCustomTypeMarshaller
+import org.grails.datastore.mapping.query.Query
+import org.grails.datastore.mapping.simple.query.SimpleMapResultList
 import org.joda.time.LocalDate
+import org.grails.datastore.mapping.model.*
 import spock.lang.*
 
 @Mock(Person)
 class UnitTestSupportSpec extends Specification {
 
 	def setup() {
+		MappingFactory.registerCustomType(new SimpleMapLocalDateMarshaller())
+
 		new Person(name: "Alex", birthday: new LocalDate(2008, 10, 2)).save(failOnError: true)
 		new Person(name: "Nicholas", birthday: new LocalDate(2010, 11, 14)).save(failOnError: true)
 	}
@@ -28,11 +34,11 @@ class UnitTestSupportSpec extends Specification {
 	def "can use criteria queries on #type properties"() {
 		when:
 		def results = Person.withCriteria {
-			gt "birthday", new LocalDate(2010, 1, 1)
+			gt "birthday", new LocalDate(2008, 1, 1)
 		}
 
 		then:
-		results.name == ["Nicholas"]
+		results.name == ["Alex", "Nicholas"]
 	}
 
 	def "metadata for #type properties is correct"() {
@@ -58,13 +64,45 @@ class UnitTestSupportSpec extends Specification {
 
 	def "persistent entity looks right"() {
 		given:
-		println DomainClassUnitTestMixin.simpleDatastore.mappingContext.mappingSyntaxStrategy.propertyFactory
-		def entity = DomainClassUnitTestMixin.simpleDatastore.mappingContext.getPersistentEntity(Person.name)
+		def mappingContext = DomainClassUnitTestMixin.simpleDatastore.mappingContext
+		def entity = mappingContext.getPersistentEntity(Person.name)
 		def prop = entity.getPropertyByName("birthday")
 
 		expect:
 		prop != null
 		prop.type == LocalDate
+	}
+
+}
+
+class SimpleMapLocalDateMarshaller extends AbstractMappingAwareCustomTypeMarshaller<LocalDate, Map, SimpleMapResultList> {
+
+	SimpleMapLocalDateMarshaller() {
+		super(LocalDate)
+	}
+
+	@Override
+	protected Object writeInternal(PersistentProperty property, String key, LocalDate value, Map nativeTarget) {
+		nativeTarget[key] = value
+	}
+
+	@Override
+	protected LocalDate readInternal(PersistentProperty property, String key, Map nativeSource) {
+		nativeSource[key]
+	}
+
+	@Override
+	protected void queryInternal(PersistentProperty property, String key, Query.PropertyCriterion criterion, SimpleMapResultList nativeQuery) {
+		switch (criterion) {
+			case Query.Equals:
+				nativeQuery.results << nativeQuery.query.handlers[Query.Equals].call(criterion, property, null)
+				break
+			case Query.GreaterThan:
+				nativeQuery.results << nativeQuery.query.handlers[Query.GreaterThan].call(criterion, property, null)
+				break
+			default:
+				throw new RuntimeException("unsupported query type $criterion for property $property")
+		}
 	}
 
 }
