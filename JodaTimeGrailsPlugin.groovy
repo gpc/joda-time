636f5bccc3b26431388803e5ffdc496ef7025ddb
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-11-02 22:44:04 +0000

    updated plugin metadata

--- JodaTimeGrailsPlugin.groovy
+++ JodaTimeGrailsPlugin.groovy
@@ -13,49 +13,52 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import com.energizedwork.grails.plugins.jodatime.*
+
+import grails.plugin.jodatime.JodaDynamicMethods
 import grails.plugin.jodatime.binding.JodaTimePropertyEditorRegistrar
 import grails.plugin.jodatime.converters.JodaConverters
-import grails.plugin.jodatime.JodaDynamicMethods
 
 class JodaTimeGrailsPlugin {
-    def version = "1.3.BUILD-SNAPSHOT"
-	def grailsVersion = "1.3 > *"
-    def dependsOn = [core: "1.3 > *", converters: "1.3 > *"]
 
-    def author = "Rob Fletcher"
-    def authorEmail = "rob@energizedwork.com"
-    def title = "Joda-Time Plugin"
-    def description = '''\\
+	def version = "1.3.BUILD-SNAPSHOT"
+	def grailsVersion = "1.3 > *"
+	def dependsOn = [core: "1.3 > *", converters: "1.3 > *"]
+
+	def title = "Joda-Time Plugin"
+	def description = '''\\
 The Joda-Time Plugin integrates the <a href="http://joda-time.sourceforge.net/">Joda Time</a> date/time library into
 Grails. The plugin provides:
 <ul>
-<li>Libraries necessary to use Joda Time types as persistent fields on domain classes.</li>
+<li>Support for Joda Time types as persistent fields on domain classes including in Grails 2 unit tests.</li>
 <li>The ability to bind from form inputs to Joda Time fields on domain or command objects.</li>
 <li>JSON and XML rendering of Joda Time types.</li>
-<li>Tag-libs for input and output of Joda Time data.</li>
+<li>Tag-libs for input and output of Joda Time values.</li>
 <li>Enhancements to Grails scaffolding to support domain classes with Joda Time fields.</li>
 <li>Compatibility and consistency methods added to Joda Time types.</li>
 </ul>
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/JodaTime+Plugin"
 
-    def pluginExcludes = [
-		'grails-app/controllers/**',
-		'grails-app/domain/**',
-		'grails-app/views/**',
-		'grails-app/i18n/**',
-		'web-app/**'
+	def license = "APACHE"
+	def developers = [[name: "Rob Fletcher", email: "rob@energizedwork.com"]]
+	def documentation = "http://grails.org/JodaTime+Plugin"
+	def issueManagement = [system: "JIRA", url: "http://jira.grails.org/browse/GPJODATIME"]
+	def scm = [url: "https://github.com/gpc/grails-joda-time/"]
+
+	def pluginExcludes = [
+			'grails-app/controllers/**',
+			'grails-app/domain/**',
+			'grails-app/views/**',
+			'grails-app/i18n/**',
+			'web-app/**'
 	]
 
-    def doWithSpring = {
-        jodaTimePropertyEditorRegistrar(JodaTimePropertyEditorRegistrar)
-    }
+	def doWithSpring = {
+		jodaTimePropertyEditorRegistrar(JodaTimePropertyEditorRegistrar)
+	}
 
-    def doWithDynamicMethods = { ctx ->
-        JodaDynamicMethods.registerDynamicMethods()
+	def doWithDynamicMethods = { ctx ->
+		JodaDynamicMethods.registerDynamicMethods()
 		JodaConverters.registerJsonAndXmlMarshallers()
-    }
+	}
 }
