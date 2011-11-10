b3bf85ceba18eebc15af4a58709e47144905c502
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-11-10 12:56:34 +0000

    update dependencies for 1.3

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -2,49 +2,46 @@ grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
 grails.project.dependency.resolution = {
-	inherits "global"
-	log "warn"
-	repositories {
-		grailsPlugins()
-		grailsHome()
-		grailsCentral()
-		mavenLocal()
-		mavenCentral()
-		mavenRepo "http://repo.grails.org/grails/libs-releases"
-		mavenRepo "http://m2repo.spockframework.org/ext/"
-	}
-	dependencies {
-		compile "joda-time:joda-time:2.0"
-//		compile "org.grails:grails-datastore-simple:1.0.0.RC1"
-		test("org.hamcrest:hamcrest-all:1.1") {
-			exported = false
-		}
-//		test("org.grails:grails-datastore-gorm-test:1.0.0.RC1") {
-//			transitive = false
-//		}
-		def datastoreVersion = "1.0.0.RC1"
-		compile("org.grails:grails-datastore-gorm-mongo:$datastoreVersion",
-				"org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
-				"org.grails:grails-datastore-gorm:$datastoreVersion",
-				"org.grails:grails-datastore-core:$datastoreVersion",
-				"org.grails:grails-datastore-web:$datastoreVersion") {
-			transitive = false
-		}
+    inherits "global"
+    log "warn"
+    repositories {
+        grailsPlugins()
+        grailsHome()
+        grailsCentral()
+        mavenLocal()
+        mavenCentral()
+        mavenRepo "http://repo.grails.org/grails/libs-releases"
+        mavenRepo "http://m2repo.spockframework.org/ext/"
+    }
+    dependencies {
+        compile "joda-time:joda-time:2.0"
+        test("org.hamcrest:hamcrest-all:1.1") {
+            exported = false
+        }
+        if (grailsVersion.find(/^\d+/).toInteger() < 2) {
+            println "Grails version: $grailsVersion"
+            def datastoreVersion = "1.0.0.RC1"
+            compile("org.grails:grails-datastore-gorm-plugin-support:$datastoreVersion",
+                    "org.grails:grails-datastore-gorm:$datastoreVersion",
+                    "org.grails:grails-datastore-core:$datastoreVersion") {
+                transitive = false
+            }
 
-		test("org.grails:grails-datastore-gorm-test:$datastoreVersion",
-				"org.grails:grails-datastore-simple:$datastoreVersion") {
-			transitive = false
-		}
-	}
-	plugins {
-		build(":release:1.0.0.RC3") {
-			export = false
-		}
-		build(":svn:1.0.0.M1") {
-			export = false
-		}
-		test(":spock:0.6-SNAPSHOT") {
-			export = false
-		}
-	}
+            test("org.grails:grails-datastore-gorm-test:$datastoreVersion",
+                    "org.grails:grails-datastore-simple:$datastoreVersion") {
+                transitive = false
+            }
+        }
+    }
+    plugins {
+        build(":release:1.0.0.RC3") {
+            export = false
+        }
+        build(":svn:1.0.0.M1") {
+            export = false
+        }
+        test(":spock:0.6-SNAPSHOT") {
+            export = false
+        }
+    }
 }
--- test/projects/joda-with-grails-137/test/unit/jodatest/SimpleDatastoreSpec.groovy
+++ test/projects/joda-with-grails-137/test/unit/jodatest/SimpleDatastoreSpec.groovy
@@ -1,28 +1,31 @@
 package jodatest
 
-import spock.lang.*
-import grails.plugin.spock.*
-import grails.datastore.test.*
-import jodatest.*
-import org.joda.time.*
+import grails.datastore.test.DatastoreUnitTestMixin
+import grails.plugin.jodatime.simpledatastore.SimpleMapJodaTimeMarshaller
+import org.joda.time.LocalDate
+import spock.lang.Specification
 
 @Mixin(DatastoreUnitTestMixin)
-class SimpleDatastoreSpec extends UnitSpec {
+class SimpleDatastoreSpec extends Specification {
 
 	def "can use a dynamic finder on a joda-time property"() {
-		given:
-		new Person(name: "Rob", birthday: new LocalDate(1971, 11, 29)).save(failOnError: true)
-		new Person(name: "Alex", birthday: new LocalDate(2008, 10, 2)).save(failOnError: true)
-		new Person(name: "Nicholas", birthday: new LocalDate(2010, 11, 14)).save(failOnError: true)
-		new Person(name: "Mystery", birthday: null).save(failOnError: true)
-		
 		expect:
+        Person.count() == 4
 		Person.findByBirthday(new LocalDate(2008, 10, 2)).name == "Alex"
-		Person.findByBirthdayGreaterThan(new LocalDate(2000, 1, 1)).name == ["Alex", "Nicholas"]
+		Person.findByBirthdayGreaterThan(new LocalDate(2000, 12, 31)).name == ["Alex", "Nicholas"]
 		Person.findByBirthdayIsNull().name == "Mystery"
 	}
+
+    def setupSpec() {
+        SimpleMapJodaTimeMarshaller.initialize()
+    }
 	
 	def setup() {
+        def people = []
+        people << new Person(name: "Rob", birthday: new LocalDate(1971, 11, 29))
+        people << new Person(name: "Alex", birthday: new LocalDate(2008, 10, 2))
+        people << new Person(name: "Nicholas", birthday: new LocalDate(2010, 11, 14))
+        people << new Person(name: "Mystery", birthday: null)
 		mockDomain Person
 	}
 
