482217021dcdaaa8d1b66a1f9150c68dc63c921d
Rob Fletcher <robert.w.fletcher@gmail.com>
2011-12-08 12:00:20 +0000

    soft-load class in _Events
    Without this fix there's a silent exception thrown that prevents the unit test support from initializing

--- scripts/_Events.groovy
+++ scripts/_Events.groovy
@@ -1,6 +1,7 @@
 eventTestPhaseStart = { phase ->
 	if (phase == "unit") {
 		event "StatusUpdate", ["configuring joda-time support for simple datastore"]
-		grails.plugin.jodatime.simpledatastore.SimpleMapJodaTimeMarshaller.initialize()
+		def marshallerClass = classLoader.loadClass("grails.plugin.jodatime.simpledatastore.SimpleMapJodaTimeMarshaller")
+		marshallerClass.initialize()
 	}
 }
