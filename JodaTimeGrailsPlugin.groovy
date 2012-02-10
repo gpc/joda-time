d4a20b0f9e82d08db37cc41dbe706f0f489eaa31
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-02-10 16:12:22 +0000

    bump version no

--- JodaTimeGrailsPlugin.groovy
+++ JodaTimeGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import grails.plugin.jodatime.converters.JodaConverters
 
 class JodaTimeGrailsPlugin {
 
-	def version = "1.3.1"
+	def version = "1.4"
 	def grailsVersion = "1.3 > *"
 	def dependsOn = [core: "1.3 > *", converters: "1.3 > *"]
 
