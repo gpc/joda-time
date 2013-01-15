049066766b64654b9ea53d90632c8c65533a4b59
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-01-15 04:02:02 +0000

    upgrade test project to Grails 2.2.0 & upgrade dependencies

--- test/projects/joda-time-test/grails-app/conf/BuildConfig.groovy
+++ test/projects/joda-time-test/grails-app/conf/BuildConfig.groovy
@@ -3,8 +3,10 @@ grails.project.class.dir = 'target/classes'
 grails.project.test.class.dir = 'target/test-classes'
 grails.project.test.reports.dir = 'target/test-reports'
 grails.project.dependency.resolution = {
+
 	inherits 'global'
 	log 'warn'
+
 	repositories {
 		grailsPlugins()
 		grailsHome()
@@ -13,24 +15,25 @@ grails.project.dependency.resolution = {
 		mavenCentral()
 	}
 
-	def gebVersion = '0.7.2'
-	def seleniumVersion = '2.25.0'
+	def gebVersion = '0.9.0-RC-1'
+	def seleniumVersion = '2.28.0'
 
 	dependencies {
-		test("org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion")
-		test "org.seleniumhq.selenium:selenium-support:$seleniumVersion"
-		test "org.codehaus.geb:geb-spock:$gebVersion"
-		compile "org.jadira.usertype:usertype.jodatime:1.9"
+		test 'org.spockframework:spock-grails-support:0.7-groovy-2.0',
+				"org.seleniumhq.selenium:selenium-chrome-driver:$seleniumVersion",
+				"org.seleniumhq.selenium:selenium-support:$seleniumVersion",
+				"org.gebish:geb-spock:$gebVersion"
+		compile 'org.jadira.usertype:usertype.jodatime:1.9'
 	}
 	plugins {
 		build ":tomcat:$grailsVersion"
-		runtime ":fields:1.2"
-		runtime ":hibernate:$grailsVersion"
-		test ':build-test-data:1.1.2'
-		test ':fixtures:1.0.7'
-		test ":geb:$gebVersion"
-		test(':spock:0.6') {
-			excludes 'junit-dep', 'groovy-all'
+		runtime ":fields:1.3",
+				":hibernate:$grailsVersion"
+		test ':build-test-data:2.0.3',
+				':fixtures:1.2',
+				":geb:$gebVersion"
+		test(':spock:0.7') {
+			exclude 'spock-grails-support'
 		}
 	}
 }
