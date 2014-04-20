c13ec487f5a074f40a22480566c1d58f2915b34f
Rob Fletcher <robert.w.fletcher@gmail.com>
2014-04-20 07:02:23 +0100

    upgrade release plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -22,7 +22,7 @@ grails.project.dependency.resolution = {
 	}
 
 	plugins {
-		build ':release:2.2.0', ':rest-client-builder:1.0.3', {
+		build ':release:3.0.1', ':rest-client-builder:2.0.1', {
 			export = false
 		}
 	}
