f259c33e06e65e97cdb05345ba0d4f56b7cebf5b
Rob Fletcher <robert.w.fletcher@gmail.com>
2014-04-24 04:54:03 +0100

    remove _Events script

--- scripts/_Events.groovy
+++
@@ -1,7 +0,0 @@
-//eventTestPhaseStart = { phase ->
-//	if (phase == "unit") {
-//		event "StatusUpdate", ["configuring joda-time support for simple datastore"]
-//		def marshallerClass = classLoader.loadClass("grails.plugin.jodatime.simpledatastore.SimpleMapJodaTimeMarshaller")
-//		marshallerClass.initialize()
-//	}
-//}
