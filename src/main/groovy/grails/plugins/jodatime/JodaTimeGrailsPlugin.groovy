a895f2cca7a545e451cb99aef7d53a45d5b5b8bd
graemerocher <graeme.rocher@gmail.com>
2015-06-18 14:15:03 +0200

    cleanup

--- src/main/groovy/grails/plugins/jodatime/JodaTimeGrailsPlugin.groovy
+++ src/main/groovy/grails/plugins/jodatime/JodaTimeGrailsPlugin.groovy
@@ -93,22 +93,4 @@ Joda Time integration for Grails
         }
     }
 
-    void doWithApplicationContext() {
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    void onChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    void onConfigChange(Map<String, Object> event) {
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    void onShutdown(Map<String, Object> event) {
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
