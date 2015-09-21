efe25c0b92f174ec7d3282f0cfc68df45aabcf51
Tom Crossland <tom.crossland@bluetab.net>
2015-09-21 12:46:35 +0200

    Updated to joda 2.8.2

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -24,7 +24,7 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'joda-time:joda-time:2.3'
+		compile 'joda-time:joda-time:2.8.2'
 
     test("org.grails:grails-web-databinding-spring:$grailsVersion")
 		test('org.hamcrest:hamcrest-all:1.1') { export = false }
